7f6accb6cc919cab3916c238b07aed04589f9a9e
Sunziyue <sunziyue@live.com>
2019-05-23 17:48:57 +0800

    rebuild

--- js/email.js
+++ js/email.js
@@ -9,21 +9,21 @@
 
 function handlerEmail() {
     $('#messageInfo').show();
-    var templateParams = {
-        name: $('#name').val(),
-        email: $('#email').val(),
-        phone: $('#phone').val(),
-        message_html: $('#message').val()
-    };
-    //语法 emailjs.send(serviceID, templateID, templateParams, userID);  
-    emailjs.send('outlook', 'template_fCJeeIyk', templateParams, "user_6MAx7Wpisoox26OJNtsqJ")
-        .then(function(response) {
-            $('#messageInfo').hide();
-            $('#messageDenger').hide();
-            $('#messageSuccess').show();
-        }, function(error) {
-            $('#messageInfo').hide();
-            $('#messageSuccess').hide();
-            $('#messageDenger').show();
-        });
+    // var templateParams = {
+    //     name: $('#name').val(),
+    //     email: $('#email').val(),
+    //     phone: $('#phone').val(),
+    //     message_html: $('#message').val()
+    // };
+    // //语法 emailjs.send(serviceID, templateID, templateParams, userID);  
+    // emailjs.send('outlook', 'template_fCJeeIyk', templateParams, "user_6MAx7Wpisoox26OJNtsqJ")
+    //     .then(function(response) {
+    //         $('#messageInfo').hide();
+    //         $('#messageDenger').hide();
+    //         $('#messageSuccess').show();
+    //     }, function(error) {
+    //         $('#messageInfo').hide();
+    //         $('#messageSuccess').hide();
+    //         $('#messageDenger').show();
+    //     });
 }
